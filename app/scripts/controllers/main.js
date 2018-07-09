f0db84a16db66d7f06a404db248c5c0357349746
Chris Stevens <cjs78@hotmail.co.uk>
2018-07-09 10:46:12 +0100

    initial commit
    source files

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cjsAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cjsAngularApp
+ */
+angular.module('cjsAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
